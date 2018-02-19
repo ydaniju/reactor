71b8edfdc01991119e44c4b0b99c966d8e5c8c38
Yusuf Daniju <danijuyusuf@outlook.com>
2018-02-19 16:03:51 +0100

    prevent mutation

--- src/reducers/addCounter.js
+++ src/reducers/addCounter.js
@@ -9,6 +9,14 @@ const removeCounter = (list, index) => {
   return [...list.slice(0, index), ...list.slice(index + 1)];
 }
 
+const incrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    list[index] + 1,
+    ...list.slice(index + 1)
+  ];
+}
+
 const testAddCounter = () => {
   const listBefore = [];
   const listAfter = [0];
@@ -27,6 +35,15 @@ const testRemoveCounter = () => {
   expect(removeCounter(listBefore, 1)).toEqual(listAfter);
 }
 
+const testIncrementCounter = (list, index) => {
+  const listBefore = [0, 0, 0];
+  const listAfter = [0, 1, 0];
+
+  deepFreeze(listBefore);
+  expect(incrementCounter(listBefore, 1)).toEqual(listAfter);
+}
+
 testAddCounter();
 testRemoveCounter();
+testIncrementCounter();
 console.log("We passed");
